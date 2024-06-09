05fd8dde7ebfa234080de0e3193c9e541c6fa48d
habib27akbar <akbarca94@gmail.com>
2024-06-09 16:26:22 +0700

    revisi

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,6 +23,8 @@ class AuthController extends Controller
             'captcha' => 'required|captcha'
         ]);
 
+        dd($credentials);
+
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
             return redirect()->intended('/home');
