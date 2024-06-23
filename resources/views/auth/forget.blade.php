ff01faa2f22b529a1c4d86f09d692a43106ae95d
habib27akbar <akbarca94@gmail.com>
2024-06-23 19:03:18 +0700

    change tittle

--- resources/views/auth/forget.blade.php
+++ resources/views/auth/forget.blade.php
@@ -13,7 +13,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SPRINT | LOG IN</title>
+    <title>SPRINT | FORGET PASSWORD</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
